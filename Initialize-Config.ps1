bc0eea3a7b6322fd25b2061fe55d0daf521f4b79
Brent Kuzmanich <bkuzmanich@suvoda.com>
2019-01-17 12:14:49 -0800

    - Updated to include support for TM configuration

--- Initialize-Config.ps1
+++ Initialize-Config.ps1
@@ -10,6 +10,12 @@ $config = Get-Content "$scriptDir\config.json" -Raw | ConvertFrom-Json
 
 Write-Host "Target: $studyDir"
 
+function Exit-With-Message([string]$message){
+    Write-Host $message
+    Read-Host "Press any key to exit"
+    exit;
+}
+
 function Validate-Path(
     [string]$path,
     [string]$message = ""
@@ -18,9 +24,7 @@ function Validate-Path(
     if((Test-Path $path) -eq $false)
     {
         $message = if($message -ne "") { $message } else { "Error - The following path does not exist: $path" }
-        Write-Host $message
-        Read-Host "Press any key to exit"
-        exit
+        Exit-With-Message $message
     }
 }
 
@@ -30,8 +34,15 @@ $devDir = (Get-Item $studyDir).Parent.FullName
 #inputs
 $client = Read-Host "Enter the client name"
 $clientId = Read-Host "Enter the client Id"
-$protocol = Read-Host "Enter the study protocol"
-$version = Read-Host "Enter the IRT version"
+
+#IRT or TM?
+$tmOrIRT = Read-Host "Trial Manager or IRT (TM = Trial Manager; IRT = IRT)"
+$isIRT = if($tmOrIRT.ToLower() -eq "irt") { $true } else { if($tmOrIRT.ToLower() -eq "tm") { $false } else { Exit-With-Message "Only Trial Manager and IRT are supported" }}
+
+if($isIRT -eq $true) {
+    $protocol = Read-Host "Enter the study protocol"
+}
+$version = if($isIRT -eq $true) { Read-Host "Enter the IRT version" } else { Read-Host "Enter the Trial Manager version" }
 $forceOverwrite = Read-Host "Overwrite existing config files (y/n)"
 $force = if($forceOverwrite.ToLower() -eq "y") { $true } else { $false }
 
@@ -40,7 +51,8 @@ $versionParts = $version.Split(".")
 $version = $versionParts[0] + "." + $versionParts[1]
 
 $templateName = "template_$version.config"
-$templatePath = "$scriptDir\" + $config.templatesSubDir + "\$templateName"
+$templateSubDir = if($isIRT -eq $true) { $config.irtTemplatesSubDir } else { $config.tmTemplatesSubDir }
+$templatePath = "$scriptDir\$templateSubDir\$templateName"
 Validate-Path $templatePath "Error - IRT version not supported"
 
 $username = $env:USERNAME
@@ -59,13 +71,11 @@ $replaceMap = @{
 
 
 #App.config
-if ((Test-Path $userSettingsDir) -eq $false -or $force -eq $true)
-{
+if ((Test-Path $userSettingsDir) -eq $false -or $force -eq $true) {
     #add user settings directory
     New-Item -Path $userSettingsDir -ItemType directory -Force
 
-    if ((Test-Path $appConfigPath) -eq $false -or $force -eq $true)
-    {
+    if ((Test-Path $appConfigPath) -eq $false -or $force -eq $true) {
         #copy default App.config into user settings dir
         Copy-Item $templatePath $appConfigPath -Force
     
@@ -90,15 +100,13 @@ $emailPickupDirectoryPathNode = $appConfigXml | Select-Xml -XPath $xPath
 $studyEmailDumpDir = $emailPickupDirectoryPathNode.Node.value
 
 
-if((Test-Path $studyEmailDumpDir) -eq $false)
-{
+if((Test-Path $studyEmailDumpDir) -eq $false) {
     New-Item -Path $studyEmailDumpDir -ItemType directory
 }
 
 
 #.setty
-if((Test-Path $settyPath) -eq $false)
-{
+if((Test-Path $settyPath) -eq $false) {
     New-Item -Path $settyPath -ItemType file
     Set-Content -Path $settyPath -Value ($config.settingsSubDir +"$username")
 }
